d5a114f27e21ed547aaa1b6f5f4db49d0c6c4f6a
BassTheCoder <patryk.nowak.hello@gmail.com>
2023-01-03 15:14:50 +0100

    little refactoring - methods extraction

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -19,20 +19,32 @@ public class PlayerScript : MonoBehaviour
         float xAxis = Input.GetAxisRaw("Horizontal");
         float yAxis = Input.GetAxisRaw("Vertical");
 
-        //Reset move
-        _moveDelta = new Vector3(xAxis, yAxis, 0);
+        ResetMoveVector(xAxis, yAxis);
 
-        //sprite orientation based on movement
-        if (_moveDelta.x > 0)
+        OrientatePlayerModelOnMovement(_moveDelta.x);
+
+        MovePlayer(_moveDelta);
+    }
+
+    private void ResetMoveVector(float x, float y)
+    {
+        _moveDelta = new Vector3(x, y, 0);
+    }
+
+    private void OrientatePlayerModelOnMovement(float x)
+    {
+        if (x > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
-        else if (_moveDelta.x < 0)
+        else if (x < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
+    }
 
-        //move
-        transform.Translate(_moveDelta * Time.deltaTime);
+    private void MovePlayer(Vector3 vector)
+    {
+        transform.Translate(vector * Time.deltaTime);
     }
 }
