2a8854c5dc8c98e7f637719d4c842f39ebb0e02c
BassTheCoder <patryk.nowak.hello@gmail.com>
2023-01-04 02:13:55 +0100

    initial camera script refactor

--- Assets/Scripts/Camera_FollowPlayer.cs
+++ Assets/Scripts/Camera_FollowPlayer.cs
@@ -8,11 +8,18 @@ public class Camera_FollowPlayer : MonoBehaviour
 
     void Update()
     {
+        var vector = GetVectorFromPlayerToMouse();
+        transform.position = PlayerPosition.position + new Vector3(vector.x, vector.y, -1);
+    }
+
+    private Vector3 GetVectorFromPlayerToMouse() //todo: make it prettier
+    {
         var mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, PlayerPosition.position.z));
         var direction = mousePosition - PlayerPosition.position;
         direction.Normalize();
         var distance = direction.magnitude;
-        var vector = direction/distance;
-        transform.position = PlayerPosition.position + new Vector3(vector.x/4, vector.y/4, -1);
+        var vector = direction / distance;
+
+        return vector/4;
     }
 }
