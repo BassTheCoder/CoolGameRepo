a6ccdd502dcf967677bd0a8ab5a394e2dacbc82b
BassTheCoder <patryk.nowak.hello@gmail.com>
2023-02-05 02:37:00 +0100

    proper scripted door prefab

--- Assets/Scripts/EnvironmentScripts/DoorScript.cs
+++ Assets/Scripts/EnvironmentScripts/DoorScript.cs
@@ -1,12 +1,20 @@
+using System.Linq;
 using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
+    public EnemySpawner[] SpawnersClearedRequiredToOpen = new EnemySpawner[0];
+    public bool IsLevelExit = false;
+
     private void Update()
     {
-        if (GameObject.FindGameObjectWithTag("GameController").GetComponent<WinCondition>().IsLevelFinished)
+        if (CheckIfShouldOpen())
         {
             Destroy(gameObject);
         }
     }
+
+    private bool CheckIfShouldOpen() =>
+        (IsLevelExit && GameObject.FindGameObjectWithTag("GameController").GetComponent<WinCondition>().IsLevelFinished) ||
+        (SpawnersClearedRequiredToOpen.Length > 0 && SpawnersClearedRequiredToOpen.All(spawner => spawner.IsRoomFinished));
 }
